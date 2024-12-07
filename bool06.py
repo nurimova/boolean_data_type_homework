915739dcacd645cfff8c8c4d892dd4516c769951
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-12-07 16:29:15 +0200

    Update bool06.py

--- bool06.py
+++ bool06.py
@@ -1,10 +1,10 @@
 def main(a):
     """
-    check the following statement "The variable "a" is an even number"
-    Args:
+    Quyidagi bayonotni tekshiring: "O‘zgaruvchi 'a' juft son"
+    Berilganlar:
         a: int
-    Returns:
+    Qaytaradi:
         bool
     """
-    # Write your code here
-    return 
+    # Kodni shu yerga yozing
+    return
